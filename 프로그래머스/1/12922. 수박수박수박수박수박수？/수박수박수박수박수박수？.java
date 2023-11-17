33f7f157f49856a69ccd22963632c19afbdf432b
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2023-11-18 00:05:54 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public StringBuilder solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<n; i++) {
+            if (i%2 == 0) sb.append("수");
+            else sb.append("박");
+        }
+        return sb;
+    }
+}
