3e9d8329d45f63ec9cc6e8402035e9e877718eb3
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2023-11-24 01:57:16 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+    for b in range(1,total+1):
+        if (total / b) % 1 == 0:
+            a = total / b
+            if 2*a + 2*b == brown + 4:
+                return [a,b]
