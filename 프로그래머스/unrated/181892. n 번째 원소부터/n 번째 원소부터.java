749d7ee37e0e50af5604d465495db9d029ec3e2c
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2023-12-19 00:48:58 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] num_list, int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i=n-1; i<num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+        return answer;
+    }
+}
