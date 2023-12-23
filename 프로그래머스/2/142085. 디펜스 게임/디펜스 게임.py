cab6b36db994fd21a59dee770cf79acbdbddd3f4
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2023-12-23 23:13:07 +0900

    [level 2] Title: 디펜스 게임, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,17 @@
+from heapq import heappush, heappop
+
+def solution(n, k, enemy):
+    stage = len(enemy)
+    if k >= stage :
+        return stage
+    q = []
+    
+    for i in range(stage) :
+        heappush(q, enemy[i])
+        if len(q) > k :
+            last = heappop(q)
+            if last > n :
+                return i
+            n -= last
+        
+    return stage
