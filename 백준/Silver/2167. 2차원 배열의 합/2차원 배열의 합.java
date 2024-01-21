71a763f0e6dce552a47e1c5aa21960e980eff660
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2024-01-21 12:11:58 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1172 ms, Memory: 47324 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+
+        while(K-- > 0) {
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int a = i - 1; a < x; a++) {
+                for (int b = j - 1; b < y; b++) {
+                    sum += map[a][b];
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
