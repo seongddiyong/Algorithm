de5586829cd00868fa3c3c0d91e2d3d428711361
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2024-01-23 00:19:19 +0900

    [Silver IV] Title: 빙고, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -49,9 +49,10 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        // 해당 값의 좌표가 어딘지 빠르게 찾으려고 파이썬 dictionary 처럼 사용했습니다.
         Map<Integer, int[]> d = new HashMap<>();
 
-        int[][] map = new int[5][5];
+        // false, true로 체크가 되었는지 확인하는 배열
         checked = new boolean[5][5];
 
         int num = 0;
@@ -59,15 +60,16 @@ class Main {
         int x = 0;
         int y = 0;
 
+        // 해당 값이 어느 좌표였는지 기록
         for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5; j++) {
                 num = Integer.parseInt(st.nextToken());
-                map[i][j] = num;
                 d.put(num, new int[] {i,j});
             }
         }
 
+        // 루프 설정 (한 번에 빠져나오기)
         loop:
         for (int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
@@ -77,12 +79,14 @@ class Main {
                 y = d.get(num)[1];
                 checked[x][y] = true;
                 cnt++;
-                if (cnt >= 5) {
+                // 걍 cnt 12이상이어야 3빙고니까
+                if (cnt >= 12) {
                     checkTypeOne();
                     checkTypeTwo();
                     checkTypeThree();
                     checkTypeFour();
                 }
+                // 빙고 3개면 (2 초과) 출력 후 끝냄
                 if (bingoCnt > 2) {
                     System.out.println(cnt);
                     break loop;
