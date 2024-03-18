0b2cefbddcb9089d4b8d2272b58a80cccb391382
seongddiyong <89764169+seongddiyong@users.noreply.github.com>
2024-03-18 22:39:11 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -11,10 +11,10 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         int [][] dp = new int[N+1][N+1];
-        
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j <= i; j++) {
-                if (j == 0) {
+                if (j == 0 || j == i) {
                     dp[i][j] = 1;
                 } else {
                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
