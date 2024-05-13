1c3767e4091a2d9a5e00202c4aac30d6c8854c02
박성훈 <89764169+seongddiyong@users.noreply.github.com>
2024-05-13 22:01:34 +0900

    [Gold II] Title: 로봇 조종하기, Time: 516 ms, Memory: 79456 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -24,15 +24,17 @@ class Main {
         for (int i = 1; i < M; i++) {
             dp[0][i] = dp[0][i - 1] + map[0][i];
         }
+        
+        int [] left, right;
 
         for(int i = 1; i < N; i++){
-            int[] left = new int[M];
+            left = new int[M];
             left[0] = map[i][0] + dp[i-1][0];
             for (int j = 1; j < M; j ++){
                 left[j] = Math.max(dp[i-1][j], left[j-1]) + map[i][j];
             }
 
-            int[] right = new int[M];
+            right = new int[M];
             right[M-1] = map[i][M-1] + dp[i-1][M-1];
             for (int j = M-2; j >= 0; j --){
                 right[j] = Math.max(dp[i-1][j], right[j+1]) + map[i][j];
